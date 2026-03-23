41982c4ea7fb43032fd576075d9b32bd550d807e
Nick Tem <nola234@icloud.com>
2026-03-23 15:29:46 +0100

    Improve movie.py readability

--- models/movie.py
+++ models/movie.py
@@ -7,4 +7,9 @@ class Movie:
         self.imdb_url = imdb_url
  
     def __str__(self):
-        return f'{self.title}, {self.release_year}, {self.genre}, {self.imdb_url}'
+        return (
+            f'Title: {self.title}\n'
+            f'Year:  {self.release_year}\n'
+            f'Genre: {self.genre}\n'
+            f'IMDB:  {self.imdb_url}'
+        )
