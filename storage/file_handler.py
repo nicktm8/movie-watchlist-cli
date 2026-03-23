6da872a5b5b0d78100d02bc14cf51c8e0985578f
Nick Tem <nola234@icloud.com>
2026-03-23 17:20:43 +0100

    Implement file handler with save and load methods

---
+++ storage/file_handler.py
@@ -0,0 +1,17 @@
+import os
+import pickle
+
+class FileHandler:
+    
+    def __init__(self):
+        self.file_path = 'data/library.pkl'
+        
+    def file_save(self, movies):
+        with open(self.file_path, 'wb') as file:
+            pickle.dump(movies, file)
+    
+    def file_load(self):
+        if os.path.exists(self.file_path):
+            with open(self.file_path, 'rb') as file:
+                return pickle.load(file)
+        return []
